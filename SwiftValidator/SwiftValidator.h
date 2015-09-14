6580200e1acb0cd58aa7480b3b63da2c856e6342
Adam Preble <preble@gmail.com>
2015-09-14 14:21:52 -0400

    Adds SwiftValidator framework target (empty).

---
+++ SwiftValidator/SwiftValidator.h
@@ -0,0 +1,18 @@
+//
+//  SwiftValidator.h
+//  SwiftValidator
+//
+//  Copyright © 2015 jpotts18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftValidator.
+FOUNDATION_EXPORT double SwiftValidatorVersionNumber;
+
+//! Project version string for SwiftValidator.
+FOUNDATION_EXPORT const unsigned char SwiftValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftValidator/PublicHeader.h>
+
+
